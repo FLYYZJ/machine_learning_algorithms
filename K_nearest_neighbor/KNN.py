794c65aba12aa6fbb88939d2f7e07e5de0ad1d68
undersunshine <703076012@qq.com>
2017-05-17 11:25:19 +0800

    KNN算法实现

---
+++ K_nearest_neighbor/KNN.py
@@ -0,0 +1,38 @@
+import numpy as np
+import operator
+
+def Knn_Classifier(inX, dataSet, labels, k):
+    """
+    Knn_Classifier k近邻核心实现代码
+    :param inX: 输入待测试实例
+    :param dataSet: 输入的训练数据集
+    :param labels: 训练数据集对应的标签
+    :param k: 分为k类
+    :return: 返回对应的类别
+    """
+    dataSetSize = dataSet.shape[0]  # 获取数据集的
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet  # 计算inx与训练数据集中所有数据的差值
+    sqDiffMat = diffMat**2  # 这里采用的是欧氏距离
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = np.argsort(distances)  # 按照欧氏距离的大小进行排序
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]  # 依次存入欧氏距离前k个最小距离的编号
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1  # 对应一个字典
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)  # 对字典进行排序
+    return sortedClassCount[0][0]  # 最高
+
+
+def createDataSet():
+    """
+    createDataSet 小数据集生成demo
+    :return:
+    """
+    group = np.array([[1.0, 1.1], [1.0, 1.0], [0., 0.], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+if __name__ == "__main__":
+    group, lables = createDataSet()
+    print(Knn_Classifier([0,0], group, lables, 3))
