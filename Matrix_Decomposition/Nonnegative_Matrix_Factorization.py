8b01d3c308d4d0b5323cee04ab8ae093ee78d888
yao <703076012@qq.com>
2017-06-19 18:39:09 +0800

    非负矩阵分解

---
+++ Matrix_Decomposition/Nonnegative_Matrix_Factorization.py
@@ -0,0 +1,65 @@
+# @Time    : 2017/6/18 21:30
+# @Author  : Aries
+# @Site    :
+# @File    : Nonnegative_Matrix_Factorization.py
+# @Software: PyCharm Community Edition
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+n_row = 2
+n_col = 4
+image_shape = (64, 64)
+
+
+def train(V, r, k, e):
+    m,n = V.shape
+    W = np.random.random((m, r))
+    H = np.random.random((r, n))
+    for iter in range(k):
+        V_pre = np.dot(W, H)
+        E = V - V_pre
+
+        err = np.sum(E * E)
+        print(err)
+        if err < e:
+            break
+
+        a = np.dot(W.T, V)
+        b = np.dot(W.T, np.dot(W, H))
+        H[b != 0] = (H * a / b)[b != 0]
+
+        c = np.dot(V, H.T)
+        d = np.dot(W, np.dot(H, H.T))
+
+        W[d != 0] = (W * c / d)[d != 0]
+    return W, H
+
+
+
+
+def plot_gallery(title, images, n_col=n_col, n_row=n_row):
+    plt.figure(figsize=(2. * n_col, 2.26 * n_row))
+    plt.suptitle(title, size=16)
+    for i, comp in enumerate(images):
+        plt.subplot(n_row, n_col, i + 1)
+        vmax = max(comp.max(), -comp.min())
+        plt.imshow(comp.reshape(image_shape).T, cmap=plt.cm.gray,
+                   interpolation='nearest',
+                   vmin=-vmax, vmax=vmax)
+        plt.xticks(())
+        plt.yticks(())
+    plt.subplots_adjust(0.01, 0.05, 0.99, 0.93, 0.04, 0.)
+
+if __name__ == '__main__':
+    data = pd.read_csv('data.csv', sep='\t', header=None).values.T
+
+    W, H = train(data, 8, 1000, 1e-4)
+    print(H)
+    print('**********************')
+    print(W)
+    plot_gallery('%s - Train time %.1fs' % ('Non-negative components - NMF', 1),
+                 H)
+    plt.show()
+
---
+++ Tree/Decision_Tree_Classification.py
@@ -0,0 +1,31 @@
+# @Time    : 2017/6/1 11:00
+# @Author  : Aries
+# @Site    : 
+# @File    : Decision_Tree_Classification.py
+# @Software: PyCharm Community Edition
+
+import pandas as pd
+import numpy as np
+
+class Node(object):
+    def __init__(self, name, is_leaf, label):
+        self.name = name
+        self.is_leaf = is_leaf
+        self.label = label
+        self.child = dict()
+
+
+
+def createDataset():
+    dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    label = ['no surfacion', 'flippers']
+def Load_Data(file_path = ''):
+    if file_path == '':
+        print('自动生成随机数据集，一个二元分类的简单问题')
+        dataSet, label = createDataset()
+        return dataSet, label
+    else:
+        return
+
+
+
